cc5be45117c45bb69e94240c7d92ca4203be86a7
17231164 <yuanjie_ding@buaa.edu.cn>
2019-03-10 13:18:32 +0800

    retry

--- src/fibo.c
+++ src/fibo.c
@@ -6,10 +6,10 @@ int ans[30];
 int fibo(){
 	int i;
 	ans[0] = ans[1] = 1;
-	for(i = 2; i < N; ++i) {
+	for (i = 2; i < N; ++i) {
 		ans[i] = ans[i-1]+ans[i-2];
 	}
-	for(i = 0; i < N; ++i) {
+	for (i = 0; i < N; ++i) {
 		printf("%d ", ans[i]);
 	}
 }
