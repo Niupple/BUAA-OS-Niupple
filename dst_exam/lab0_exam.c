3a72392aca7705e65491d8bc39eb8256285657e5
10.210.0.67 <10.210.0.67@example.com>
2019-03-11 14:41:17 +0800

    Initial lab0-Extra

--- dst_exam/lab0_exam.c
+++
@@ -1,2 +0,0 @@
-dir/dir4/lab0_x
-dir/dir2/lab0_2:1:blablablablab hello OS lab0 alkfjalkjflakdfjlakjndf
--- dst_exam/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-find dir/ -name lab0_x > lab0_exam.c
-grep -n "hello OS lab0" dir/*/* >> lab0_exam.c
