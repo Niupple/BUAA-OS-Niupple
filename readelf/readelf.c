d04efbcb1aaa9bc3e38aa5777531f673d7fc0bd9
17231164 <yuanjie_ding@buaa.edu.cn>
2019-03-18 14:18:16 +0800

    trial 1

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -60,7 +60,7 @@ int readelf(u_char *binary, int size)
 
         // get section table addr, section header number and section header size.
 
-		ptr_sh_table = (u_char *)(binary+ehdr->e_shoff);
+		ptr_sh_table = (binary+ehdr->e_shoff);
 		sh_entry_size = ehdr->e_shentsize;
 		sh_entry_count = ehdr->e_shnum;
 
@@ -68,7 +68,7 @@ int readelf(u_char *binary, int size)
 		for (Nr = 0; Nr < sh_entry_count; ++Nr) {
 			int off = Nr*sh_entry_size;
 			shdr = ((Elf32_Shdr *)(ptr_sh_table+off));
-			printf("%d:0x%x\n", Nr, shdr->sh_addr);
+			printf("%d:0x%x\n", Nr, shdr->sh_offset);
 		}
 
         return 0;
