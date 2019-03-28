c3f79166cfd0ab258e430a368cf6dac20c0cfdfc
17231164 <yuanjie_ding@buaa.edu.cn>
2019-03-28 14:54:48 +0800

    printf Test

--- lib/print.c
+++ lib/print.c
@@ -26,310 +26,310 @@ static const char theFatalMsg[] = "fatal error in lp_Print!";
 /* -*-
  * A low level printf() function.
  */
-void
+	void
 lp_Print(void (*output)(void *, char *, int), 
-	 void * arg,
-	 char *fmt, 
-	 va_list ap)
+		void * arg,
+		char *fmt, 
+		va_list ap)
 {
 
 #define 	OUTPUT(arg, s, l)  \
-  { if (((l) < 0) || ((l) > LP_MAX_BUF)) { \
-       (*output)(arg, (char*)theFatalMsg, sizeof(theFatalMsg)-1); for(;;); \
-    } else { \
-      (*output)(arg, s, l); \
-    } \
-  }
-    
-    char buf[LP_MAX_BUF];
-
-    char c;
-    char *s;
-    long int num;
-
-    int longFlag;
-    int negFlag;
-    int width;
-    int prec;
-    int ladjust;
-    char padc;
-
-    int length;
-	int state = 0;
-
-    for(; *fmt; ++fmt) {
-	{ 
-		if (state == 0) {
-			if (*fmt != '%') {
-				OUTPUT(arg, fmt, 1);
-			} else {
-				state = 1;
-				ladjust = 0;
-				padc = ' ';
-				prec = 0;
-				width = 0;
-				longFlag = 0;
-			}
-			continue;
-		} else if(state == 1) {
-			if(*fmt == '-') {
-				ladjust = 1;
-			} else if(*fmt == '0') {
-				padc = '0';
-			} else {
-				state = 2;
-				--fmt;
-			}
-			continue;
-		} else if(state == 2) {
-			if(IsDigit(*fmt)) {
-				width = width*10+Ctod(*fmt);
-			} else {
-				--fmt;
-				state = 3;
-			}
-			continue;
-		} else if(state == 3) {
-			if(*fmt == '.') {
-				state = 4;
-			} else {
-				--fmt;
-				state = 5;
-			}
-			continue;
-		} else if(state == 4) {
-			if(IsDigit(*fmt)) {
-				prec = prec*10+Ctod(*fmt);
-			} else {
-				--fmt;
-				state = 5;
-			}
-			continue;
-		} else if(state == 5) {
-			if(*fmt == 'l') {
-				longFlag = 1;
-			} else {
-				--fmt;
-			}
-			state = 6;
-			continue;
-		}
-		state = 0;
-
-	    /* scan for the next '%' */
-	    /* flush the string found so far */
-	    /* are we hitting the end? */
+	{ if (((l) < 0) || ((l) > LP_MAX_BUF)) { \
+											   (*output)(arg, (char*)theFatalMsg, sizeof(theFatalMsg)-1); for(;;); \
+										   } else { \
+											   (*output)(arg, s, l); \
+										   } \
 	}
 
-	/* we found a '%' */
-	
-	/* check for long */
+	char buf[LP_MAX_BUF];
 
-	/* check for other prefixes */
+	char c;
+	char *s;
+	long int num;
 
-	/* check format flag */
-	negFlag = 0;
-	switch (*fmt) {
-	 case 'b':
-	    if (longFlag) { 
-		num = va_arg(ap, long int); 
-	    } else { 
-		num = va_arg(ap, int);
-	    }
-	    length = PrintNum(buf, num, 2, 0, width, ladjust, padc, 0);
-	    OUTPUT(arg, buf, length);
-	    break;
+	int longFlag;
+	int negFlag;
+	int width;
+	int prec;
+	int ladjust;
+	char padc;
 
-	 case 'd':
-	 case 'D':
-	    if (longFlag){ 
-		num = va_arg(ap, long int);
-	    } else { 
-		num = va_arg(ap, int); 
-	    }
-	    if (num < 0) {
-		num = - num;
-		negFlag = 1;
-	    }
-	    length = PrintNum(buf, num, 10, negFlag, width, ladjust, padc, 0);
-	    OUTPUT(arg, buf, length);
-	    break;
+	int length;
+	int state = 0;
 
-	 case 'o':
-	 case 'O':
-	    if (longFlag) { 
-		num = va_arg(ap, long int);
-	    } else { 
-		num = va_arg(ap, int); 
-	    }
-	    length = PrintNum(buf, num, 8, 0, width, ladjust, padc, 0);
-	    OUTPUT(arg, buf, length);
-	    break;
+	for(; *fmt; ++fmt) {
+		{ 
+			if (state == 0) {
+				if (*fmt != '%') {
+					OUTPUT(arg, fmt, 1);
+				} else {
+					state = 1;
+					ladjust = 0;
+					padc = ' ';
+					prec = 0;
+					width = 0;
+					longFlag = 0;
+				}
+				continue;
+			} else if(state == 1) {
+				if(*fmt == '-') {
+					ladjust = 1;
+				} else if(*fmt == '0') {
+					padc = '0';
+				} else {
+					state = 2;
+					--fmt;
+				}
+				continue;
+			} else if(state == 2) {
+				if(IsDigit(*fmt)) {
+					width = width*10+Ctod(*fmt);
+				} else {
+					--fmt;
+					state = 3;
+				}
+				continue;
+			} else if(state == 3) {
+				if(*fmt == '.') {
+					state = 4;
+				} else {
+					--fmt;
+					state = 5;
+				}
+				continue;
+			} else if(state == 4) {
+				if(IsDigit(*fmt)) {
+					prec = prec*10+Ctod(*fmt);
+				} else {
+					--fmt;
+					state = 5;
+				}
+				continue;
+			} else if(state == 5) {
+				if(*fmt == 'l') {
+					longFlag = 1;
+				} else {
+					--fmt;
+				}
+				state = 6;
+				continue;
+			}
+			state = 0;
 
-	 case 'u':
-	 case 'U':
-	    if (longFlag) { 
-		num = va_arg(ap, long int);
-	    } else { 
-		num = va_arg(ap, int); 
-	    }
-	    length = PrintNum(buf, num, 10, 0, width, ladjust, padc, 0);
-	    OUTPUT(arg, buf, length);
-	    break;
-	    
-	 case 'x':
-	    if (longFlag) { 
-		num = va_arg(ap, long int);
-	    } else { 
-		num = va_arg(ap, int); 
-	    }
-	    length = PrintNum(buf, num, 16, 0, width, ladjust, padc, 0);
-	    OUTPUT(arg, buf, length);
-	    break;
+			/* scan for the next '%' */
+			/* flush the string found so far */
+			/* are we hitting the end? */
+		}
 
-	 case 'X':
-	    if (longFlag) { 
-		num = va_arg(ap, long int);
-	    } else { 
-		num = va_arg(ap, int); 
-	    }
-	    length = PrintNum(buf, num, 16, 0, width, ladjust, padc, 1);
-	    OUTPUT(arg, buf, length);
-	    break;
+		/* we found a '%' */
 
-	 case 'c':
-	    c = (char)va_arg(ap, int);
-	    length = PrintChar(buf, c, width, ladjust);
-	    OUTPUT(arg, buf, length);
-	    break;
+		/* check for long */
 
-	 case 's':
-	    s = (char*)va_arg(ap, char *);
-	    length = PrintString(buf, s, width, ladjust);
-	    OUTPUT(arg, buf, length);
-	    break;
+		/* check for other prefixes */
 
-	 case '\0':
-	    fmt --;
-	    break;
+		/* check format flag */
+		negFlag = 0;
+		switch (*fmt) {
+			case 'b':
+				if (longFlag) { 
+					num = va_arg(ap, long int); 
+				} else { 
+					num = va_arg(ap, int);
+				}
+				length = PrintNum(buf, num, 2, 0, width, ladjust, padc, 0);
+				OUTPUT(arg, buf, length);
+				break;
 
-	 default:
-	    /* output this char as it is */
-	    OUTPUT(arg, fmt, 1);
-	}	/* switch (*fmt) */
+			case 'd':
+			case 'D':
+				if (longFlag){ 
+					num = va_arg(ap, long int);
+				} else { 
+					num = va_arg(ap, int); 
+				}
+				if (num < 0) {
+					num = - num;
+					negFlag = 1;
+				}
+				length = PrintNum(buf, num, 10, negFlag, width, ladjust, padc, 0);
+				OUTPUT(arg, buf, length);
+				break;
 
-    }		/* for(;;) */
+			case 'o':
+			case 'O':
+				if (longFlag) { 
+					num = va_arg(ap, long int);
+				} else { 
+					num = va_arg(ap, int); 
+				}
+				length = PrintNum(buf, num, 8, 0, width, ladjust, padc, 0);
+				OUTPUT(arg, buf, length);
+				break;
 
-    /* special termination call */
-    OUTPUT(arg, "\0", 1);
+			case 'u':
+			case 'U':
+				if (longFlag) { 
+					num = va_arg(ap, long int);
+				} else { 
+					num = va_arg(ap, int); 
+				}
+				length = PrintNum(buf, num, 10, 0, width, ladjust, padc, 0);
+				OUTPUT(arg, buf, length);
+				break;
+
+			case 'x':
+				if (longFlag) { 
+					num = va_arg(ap, long int);
+				} else { 
+					num = va_arg(ap, int); 
+				}
+				length = PrintNum(buf, num, 16, 0, width, ladjust, padc, 0);
+				OUTPUT(arg, buf, length);
+				break;
+
+			case 'X':
+				if (longFlag) { 
+					num = va_arg(ap, long int);
+				} else { 
+					num = va_arg(ap, int); 
+				}
+				length = PrintNum(buf, num, 16, 0, width, ladjust, padc, 1);
+				OUTPUT(arg, buf, length);
+				break;
+
+			case 'c':
+				c = (char)va_arg(ap, int);
+				length = PrintChar(buf, c, width, ladjust);
+				OUTPUT(arg, buf, length);
+				break;
+
+			case 's':
+				s = (char*)va_arg(ap, char *);
+				length = PrintString(buf, s, width, ladjust);
+				OUTPUT(arg, buf, length);
+				break;
+
+			case '\0':
+				fmt --;
+				break;
+
+			default:
+				/* output this char as it is */
+				OUTPUT(arg, fmt, 1);
+		}	/* switch (*fmt) */
+
+	}		/* for(;;) */
+
+	/* special termination call */
+	OUTPUT(arg, "\0", 1);
 }
 
 
 /* --------------- local help functions --------------------- */
-int
+	int
 PrintChar(char * buf, char c, int length, int ladjust)
 {
-    int i;
-    
-    if (length < 1) length = 1;
-    if (ladjust) {
-	*buf = c;
-	for (i=1; i< length; i++) buf[i] = ' ';
-    } else {
-	for (i=0; i< length-1; i++) buf[i] = ' ';
-	buf[length - 1] = c;
-    }
-    return length;
+	int i;
+
+	if (length < 1) length = 1;
+	if (ladjust) {
+		*buf = c;
+		for (i=1; i< length; i++) buf[i] = ' ';
+	} else {
+		for (i=0; i< length-1; i++) buf[i] = ' ';
+		buf[length - 1] = c;
+	}
+	return length;
 }
 
-int
+	int
 PrintString(char * buf, char* s, int length, int ladjust)
 {
-    int i;
-    int len=0;
-    char* s1 = s;
-    while (*s1++) len++;
-    if (length < len) length = len;
+	int i;
+	int len=0;
+	char* s1 = s;
+	while (*s1++) len++;
+	if (length < len) length = len;
 
-    if (ladjust) {
-	for (i=0; i< len; i++) buf[i] = s[i];
-	for (i=len; i< length; i++) buf[i] = ' ';
-    } else {
-	for (i=0; i< length-len; i++) buf[i] = ' ';
-	for (i=length-len; i < length; i++) buf[i] = s[i-length+len];
-    }
-    return length;
+	if (ladjust) {
+		for (i=0; i< len; i++) buf[i] = s[i];
+		for (i=len; i< length; i++) buf[i] = ' ';
+	} else {
+		for (i=0; i< length-len; i++) buf[i] = ' ';
+		for (i=length-len; i < length; i++) buf[i] = s[i-length+len];
+	}
+	return length;
 }
 
-int
+	int
 PrintNum(char * buf, unsigned long u, int base, int negFlag, 
-	 int length, int ladjust, char padc, int upcase)
+		int length, int ladjust, char padc, int upcase)
 {
-    /* algorithm :
-     *  1. prints the number from left to right in reverse form.
-     *  2. fill the remaining spaces with padc if length is longer than
-     *     the actual length
-     *     TRICKY : if left adjusted, no "0" padding.
-     *		    if negtive, insert  "0" padding between "0" and number.
-     *  3. if (!ladjust) we reverse the whole string including paddings
-     *  4. otherwise we only reverse the actual string representing the num.
-     */
+	/* algorithm :
+	 *  1. prints the number from left to right in reverse form.
+	 *  2. fill the remaining spaces with padc if length is longer than
+	 *     the actual length
+	 *     TRICKY : if left adjusted, no "0" padding.
+	 *		    if negtive, insert  "0" padding between "0" and number.
+	 *  3. if (!ladjust) we reverse the whole string including paddings
+	 *  4. otherwise we only reverse the actual string representing the num.
+	 */
 
-    int actualLength =0;
-    char *p = buf;
-    int i;
+	int actualLength =0;
+	char *p = buf;
+	int i;
 
-    do {
-	int tmp = u %base;
-	if (tmp <= 9) {
-	    *p++ = '0' + tmp;
-	} else if (upcase) {
-	    *p++ = 'A' + tmp - 10;
-	} else {
-	    *p++ = 'a' + tmp - 10;
+	do {
+		int tmp = u %base;
+		if (tmp <= 9) {
+			*p++ = '0' + tmp;
+		} else if (upcase) {
+			*p++ = 'A' + tmp - 10;
+		} else {
+			*p++ = 'a' + tmp - 10;
+		}
+		u /= base;
+	} while (u != 0);
+
+	if (negFlag) {
+		*p++ = '-';
 	}
-	u /= base;
-    } while (u != 0);
 
-    if (negFlag) {
-	*p++ = '-';
-    }
+	/* figure out actual length and adjust the maximum length */
+	actualLength = p - buf;
+	if (length < actualLength) length = actualLength;
 
-    /* figure out actual length and adjust the maximum length */
-    actualLength = p - buf;
-    if (length < actualLength) length = actualLength;
-
-    /* add padding */
-    if (ladjust) {
-	padc = ' ';
-    }
-    if (negFlag && !ladjust && (padc == '0')) {
-	for (i = actualLength-1; i< length-1; i++) buf[i] = padc;
-	buf[length -1] = '-';
-    } else {
-	for (i = actualLength; i< length; i++) buf[i] = padc;
-    }
-	    
-
-    /* prepare to reverse the string */
-    {
-	int begin = 0;
-	int end;
+	/* add padding */
 	if (ladjust) {
-	    end = actualLength - 1;
+		padc = ' ';
+	}
+	if (negFlag && !ladjust && (padc == '0')) {
+		for (i = actualLength-1; i< length-1; i++) buf[i] = padc;
+		buf[length -1] = '-';
 	} else {
-	    end = length -1;
+		for (i = actualLength; i< length; i++) buf[i] = padc;
 	}
 
-	while (end > begin) {
-	    char tmp = buf[begin];
-	    buf[begin] = buf[end];
-	    buf[end] = tmp;
-	    begin ++;
-	    end --;
-	}
-    }
 
-    /* adjust the string pointer */
-    return length;
+	/* prepare to reverse the string */
+	{
+		int begin = 0;
+		int end;
+		if (ladjust) {
+			end = actualLength - 1;
+		} else {
+			end = length -1;
+		}
+
+		while (end > begin) {
+			char tmp = buf[begin];
+			buf[begin] = buf[end];
+			buf[end] = tmp;
+			begin ++;
+			end --;
+		}
+	}
+
+	/* adjust the string pointer */
+	return length;
 }
